f45234974d76da11f51258cebfbf73d58cee74d8
tudodepende <leonasouza.dev@gmail.com>
2021-08-21 00:18:52 -0300

    refactored functions, added cards and links

--- data/text.js
+++ data/text.js
@@ -1,21 +1,86 @@
-const Texts = {
-  Header: {
-    title: 'Leona Souza'
+const defaultLanguage = 'pt'
+const urlGithub = 'https://github.com/leona-souza'
+const urlLinkedin = 'https://www.linkedin.com/in/leonasouza/'
+const urlTwitch = 'https://twitch.tv/leonadev'
+const pt = {
+  Static: {
+    header: 'Leona Souza',
+    description: 'Olá. Sou uma engenheira de software que reside em São Paulo. '+
+    'Já programei em PHP, MySQL, Java e JavaScript. Atualmente trabalho como '+
+    'desenvolvedora em React na Gamers Club e faço lives na Twitch compartilhando '+
+    'estudos, conhecimentos e aprendendo com a comunidade.'
   },
-  Menu: {
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ornare erat pulvinar rutrum interdum. Sed a tortor eu sem faucibus sodales. Cras vulputate auctor purus, volutpat elementum nibh bibendum vitae. Mauris blandit dictum lectus, vel sagittis purus imperdiet quis. Vestibulum id sollicitudin quam. Phasellus dapibus commodo tortor ac mollis. Etiam sodales enim quam, sed feugiat erat ornare eget. Vivamus elementum nec nibh vel aliquet. Curabitur tristique sollicitudin est, non dapibus ipsum viverra et. Nunc suscipit enim dui, ut venenatis ante porttitor in. Nunc ullamcorper, neque ac viverra euismod, ipsum urna sagittis eros, quis molestie elit tellus sit amet nisl.'
+  Cards: [
+    {
+      title: 'Título do card 1',
+      content: 'Conteúdo do card 1'
+    },
+    {
+      title: 'Título do card 2',
+      content: 'Conteúdo do card 2'
+    }
+  ]
+}
+const en = {
+  Static: {
+    header: 'Leona Souza',
+    description: 'Hello. I am a software engineer that lives in São Paulo '+
+    'I have programmed in PHP, MySQL, Java and JavaScript. Now I work as '+
+    'a React developer at Gamers Club and I stream at Twitch sharing my '+
+    'studies, knowledge and learning with the community.'
+  },
+  Cards: [
+    {
+      title: 'Card 1 title',
+      content: 'Card 1 content'
+    },
+    {
+      title: 'Card 2 title',
+      content: 'Card 2 content'
+    }
+  ]
+}
+
+const loadStaticFromObject = function(language, path = 'Static') {
+  for (let key in language[path]) {
+    document.getElementById(`${path}.${key}`).innerText = language[path][key]
   }
 }
 
-let populateFromObject;
-(populateFromObject = function(object, path) {
-  for (let key in object) {
-    const fullPath = `${path}.${key}`
-    
-    if (typeof object[key] === "object") {
-      populateFromObject(object[key], fullPath)
-    } else {
-      document.getElementById(fullPath).innerText = object[key]
-    }
+const loadCards = function(language, path = 'Cards') {
+  document.getElementById(path).innerHTML = ''
+
+  for (let key in language[path]) {
+    document.getElementById(path).innerHTML += `
+    <card class="ConteudoDinamico__Card">
+      <h3>${language.Cards[key].title}</h3>
+      <p>${language.Cards[key].content}</p>
+    </card>
+    `
   }
-})(Texts, 'Texts')
+}
+
+const loadLinks = function() {
+  document.getElementById('Footer.links').innerHTML = ""
+  document.getElementById('Footer.links').innerHTML += `
+    <span class="clickCursor" onclick="link(urlGithub)">Github</span>
+    <span class="clickCursor" onclick="link(urlLinkedin)">Linkedin</span>
+    <span class="clickCursor" onclick="link(urlTwitch)">Twitch</span>
+  `
+}
+
+const languageSwitch = function(language) {
+  populatePage(eval(language))
+}
+
+const link = function(destination) {
+  return window.open(destination, '_blank')
+}
+
+const populatePage = function(language) {
+  loadStaticFromObject(language)
+  loadLinks()
+  loadCards(language)
+}
+
+populatePage(eval(defaultLanguage))
