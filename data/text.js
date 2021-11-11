42ff00376ae6f91fc0088069d75d195cb87bea3a
Leona Souza <55172899+leona-souza@users.noreply.github.com>
2021-11-11 11:03:00 -0300

    feat: ptBr texts added

--- data/text.js
+++ data/text.js
@@ -11,7 +11,7 @@ const cardImage = [
 const pt = {
   Static: {
     header: 'Leona Souza',
-    description: `Olá! Sou uma engenheira de software que reside em São Paulo. 'Já programei em PHP, MySQL, Java e JavaScript. Atualmente trabalho como desenvolvedora frontend usando React na Gamers Club e faço lives na Twitch compartilhando estudos, conhecimentos e aprendendo com a comunidade.`,
+    description: `Olá! Sou uma engenheira de software que reside em São Paulo. Já programei em PHP, MySQL, Java e JavaScript. Atualmente trabalho como desenvolvedora frontend usando React na Gamers Club e faço lives na Twitch compartilhando estudos, conhecimentos e aprendendo com a comunidade.`,
     details: `Durante o final de minha adolescência comecei a programar em PHP e MySQL por uma pequena necessidade, mas acabei gostando tanto do desafio lógico que fui me aprofundando por prazer. Consequentemente os clientes foram aparecendo e trabalhei por aproximadamente 4 a 5 anos desenvolvendo sites do zero.
     Eventualmente troquei de ramo e comecei a trabalhar com caligrafia fazendo sobrescrição de convites de casamento e aniversário. Não durou muito tempo até que eu trocasse novamente de carreira, desta vez escolhendo fotografia. Minha principal atividade foi tratamento de imagens.
     Alguns anos depois a paixão pela programação voltou a me provocar e decidi voltar para o ramo com tudo, desta vez para ficar!`
@@ -26,25 +26,25 @@ const pt = {
     {
       id: 2,
       title: 'RanchoDev',
-      content: `Algum texto sobre o evento`,
+      content: `Em setembro de 2021 palestrei no RanchoDev (evento online devido à pandemia) com tópicos voltados para iniciantes.`,
       image: cardImage[1]
     },
     {
       id: 3,
       title: 'Feministech',
-      content: `Algum texto sobre o evento`,
+      content: `Feministech é uma equipe que me acolheu com muito carinho, onde conheci muitas pessoas incríveis. Esta imagem registra uma palestra que dei em uma maratona de palestras organizada pela Feministech.`,
       image: cardImage[2]
     },
     {
       id: 4,
       title: 'Twitch',
-      content: 'asd',
+      content: 'Faço lives na Twitch, onde compartilho, aprendo e desenvolvo projetos ao vivo. A agenda acaba variando muito por motivos de trabalho e estudos.',
       image: cardImage[3]
     },
     {
       id: 5,
       title: 'Mentoria',
-      content: 'asd'
+      content: 'A Feministech passou a organizar grupos gratuitos de mentoria para pessoas que se identificam no feminino e eu faço parte desse projeto como mentora. O objetivo desse grupo é ajudar as pessoas a iniciar ou evoluir na carreira. No momento o projeto está em sua fase inicial e é apenas para mentoria frontend.'
     }
   ]
 }
